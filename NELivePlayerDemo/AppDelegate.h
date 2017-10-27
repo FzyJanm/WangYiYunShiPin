bc7577d907f778adf824061045955899b4574cd1
Fanzhiyuan <18703823356@163.com>
2017-10-27 16:12:28 +0800

    Initial Commit

---
+++ NELivePlayerDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  NELivePlayerDemo
+//
+//  Created by NetEase on 15-10-10.
+//  Copyright (c) 2015年 netease. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class NELivePlayerLoginViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) NELivePlayerLoginViewController *viewController;
+
+@end
+
